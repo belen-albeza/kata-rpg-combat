a71c2959614aaf72ded64db9d119e867519bda31
Belén Albeza <belen@hey.com>
2023-01-22 17:25:53 +0100

    implement attacking

--- 2023-01-22-rust/src/character.rs
+++ 2023-01-22-rust/src/character.rs
@@ -15,11 +15,23 @@ impl Character {
     pub fn is_dead(&self) -> bool {
         return self.health == 0;
     }
+
+    pub fn attack(&self, damage: u32, other: &mut Character) {
+        other.health = other.health.saturating_sub(damage);
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn any_character() -> Character {
+        Character::new()
+    }
+
+    fn any_character_with_health(health: u32) -> Character {
+        Character { health, level: 1 }
+    }
 
     #[test]
     pub fn test_character_creation() {
@@ -30,16 +42,31 @@ mod tests {
 
     #[test]
     pub fn test_is_dead() {
-        let dead_chara = Character {
-            health: 0,
-            level: 1,
-        };
-        let alive_chara = Character {
-            health: 1,
-            level: 1,
-        };
+        let dead_chara = any_character_with_health(0);
+        let alive_chara = any_character_with_health(1);
 
         assert_eq!(dead_chara.is_dead(), true);
         assert_eq!(alive_chara.is_dead(), false);
+    }
+
+    #[test]
+    pub fn test_can_attack_others() {
+        let hero = any_character();
+        let mut other = any_character_with_health(1000);
+
+        hero.attack(100, &mut other);
+
+        assert_eq!(other.health, 900);
+    }
+
+    #[test]
+    pub fn test_health_drops_to_zero_when_receiving_damage_larger_than_health() {
+        let hero = any_character();
+        let mut other = any_character_with_health(1);
+
+        hero.attack(100, &mut other);
+
+        assert_eq!(other.health, 0);
+        assert_eq!(other.is_dead(), true);
     }
 }
