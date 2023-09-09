c415f45295c4672ddfefd6bdfa1ea1193c222b50
Belén Albeza <belen@hey.com>
2023-09-09 17:30:20 +0200

    interation one: Health component

--- 2023-09-09-ts/src/index.ts
+++ 2023-09-09-ts/src/index.ts
@@ -14,23 +14,39 @@ interface Healer {
 }
 
 export class Character implements HasHealth, DamageDealer, Healer {
-  static readonly #MAX_HEALTH = 1000;
-
-  #health: number = Character.#MAX_HEALTH;
   level: number = 1;
   attack: number = 10;
   healPower: number = 10;
 
+  #health: Health = new Health();
+
   get health(): number {
-    return this.#health;
+    return this.#health.value;
   }
 
   set health(value: number) {
-    this.#health = Math.min(Character.#MAX_HEALTH, Math.max(0, value));
+    this.#health.value = value;
   }
 
   get isAlive(): boolean {
-    return this.health > 0;
+    return this.#health.isAlive;
+  }
+}
+
+class Health {
+  static readonly #MAX_HEALTH = 1000;
+  #value: number = Health.#MAX_HEALTH;
+
+  get value(): number {
+    return this.#value;
+  }
+
+  set value(value: number) {
+    this.#value = Math.min(Health.#MAX_HEALTH, Math.max(0, value));
+  }
+
+  get isAlive(): boolean {
+    return this.value > 0;
   }
 }
 
