a8bd8e2dc9d154b3314b0268023471ce4abcf9a6
Belén Albeza <belen@hey.com>
2023-09-09 16:30:22 +0200

    iteration 1 (first rough pass)

--- 2023-09-09-ts/src/index.test.ts
+++ 2023-09-09-ts/src/index.test.ts
@@ -1,9 +1,73 @@
 import { describe, it, expect } from "vitest";
 
-import { add } from ".";
+import { Character } from ".";
 
-describe("add", () => {
-  it("adds two numbers", () => {
-    expect(add(2, 2)).toBe(4);
+describe("Character", () => {
+  it("is initialized at level 1 and 1000 health", () => {
+    const c = new Character();
+    expect(c.health).toBe(1000);
+    expect(c.level).toBe(1);
+  });
+
+  it("returns whether is alive or not", () => {
+    const c = new Character();
+    expect(c.isAlive).toBeTruthy();
+    c.health = 0;
+    expect(c.isAlive).toBeFalsy();
+  });
+
+  describe("attack", () => {
+    it("can deal damage to another character", () => {
+      const c = new Character();
+      const other = new Character();
+
+      c.dealDamage(20, other);
+
+      expect(other.health).toBe(980);
+    });
+
+    it("kills other character when damage exceeds health", () => {
+      const c = new Character();
+      const other = new Character();
+
+      c.dealDamage(2000, other);
+
+      expect(other.health).toBe(0);
+      expect(other.isAlive).toBeFalsy();
+    });
+  });
+
+  describe("healing", () => {
+    it("can heal another character", () => {
+      const c = new Character();
+      const other = new Character();
+      other.health = 900;
+
+      c.heal(20, other);
+
+      expect(other.health).toBe(920);
+    });
+
+    it("cannot heal over 1000 hp", () => {
+      const c = new Character();
+      const other = new Character();
+
+      c.heal(20, other);
+
+      expect(other.health).toBe(1000);
+    });
+
+    it("cannot heal a dead character", () => {
+      const c = new Character();
+      const other = new Character();
+      other.health = 0;
+
+      expect(other.isAlive).toBeFalsy();
+
+      c.heal(1, other);
+
+      expect(other.isAlive).toBeFalsy();
+      expect(other.health).toBe(0);
+    });
   });
 });
--- 2023-09-09-ts/src/index.ts
+++ 2023-09-09-ts/src/index.ts
@@ -1,1 +1,22 @@
-export const add = (x: number, y: number) => x + y;
+export class Character {
+  static readonly #MAX_HEALTH = 1000;
+
+  health: number = Character.#MAX_HEALTH;
+  level: number = 1;
+
+  get isAlive(): boolean {
+    return this.health > 0;
+  }
+
+  dealDamage(hp: number, target: Character): void {
+    target.health = Math.max(0, target.health - hp);
+  }
+
+  heal(hp: number, target: Character): void {
+    if (!target.isAlive) {
+      return;
+    }
+
+    target.health = Math.min(Character.#MAX_HEALTH, target.health + hp);
+  }
+}
