0c5b0dbaf100397c9537cd6dfbc312448f16071b
Belén Albeza <belen@hey.com>
2023-10-01 18:19:53 +0200

    add example program in index

--- 2023-09-09-ts/src/index.ts
+++ 2023-09-09-ts/src/index.ts
@@ -1,1 +1,21 @@
+import { AttackAction, HealingAction } from "./actions";
+import Character from "./character";
 
+const elf = new Character();
+elf.attack = 100;
+elf.healPower = 50;
+
+const goblin = new Character();
+goblin.attack = 80;
+
+const turns = [
+  new AttackAction(elf, goblin),
+  new AttackAction(goblin, elf),
+  new HealingAction(elf, elf),
+];
+
+for (const turn of turns) {
+  turn.perform();
+}
+
+console.log(`Combat ended! Elf now has ${elf.health} HP`);
