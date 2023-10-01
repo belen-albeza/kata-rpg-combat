c8f9a81970faa5b16e3d5d17b330de80bb04d472
Belén Albeza <belen@hey.com>
2023-10-01 17:21:04 +0200

    refactor actions so they don't inherit from abstract classes

--- 2023-09-09-ts/src/index.ts
+++ 2023-09-09-ts/src/index.ts
@@ -13,6 +13,8 @@ interface Healer {
   healPower: number;
 }
 
+interface Attacker extends DamageDealer, HasLevel {}
+
 interface HasLevel {
   level: number;
 }
@@ -54,39 +56,48 @@ class Health {
   }
 }
 
-abstract class CombatAction<T> {
-  protected readonly target: HasHealth & HasLevel;
-  protected readonly source: T;
-
-  constructor(source: T, target: HasHealth & HasLevel) {
-    this.target = target;
-    this.source = source;
-  }
-
-  abstract perform(): void;
+interface Action {
+  perform(): void;
 }
 
-export class AttackAction extends CombatAction<DamageDealer & HasLevel> {
+// export class AttackAction extends CombatAction<DamageDealer & HasLevel> {
+export class AttackAction implements Action {
+  #source: Attacker;
+  #target: HasHealth & HasLevel;
+
+  constructor(source: Attacker, target: HasHealth & HasLevel) {
+    this.#source = source;
+    this.#target = target;
+  }
+
   perform(): void {
-    if (this.source === (this.target as unknown)) {
+    if (this.#source === (this.#target as unknown)) {
       throw new InvalidTargetError("a character cannot target themselves");
     }
 
-    const levelDiff = this.source.level - this.target.level;
+    const levelDiff = this.#source.level - this.#target.level;
     const modifier = levelDiff >= 5 ? 1.5 : levelDiff <= -5 ? 0.5 : 1.0;
 
-    this.target.health -= this.source.attack * modifier;
+    this.#target.health -= this.#source.attack * modifier;
   }
 }
 
-export class HealingAction extends CombatAction<Healer> {
+export class HealingAction implements Action {
+  #source: Healer;
+  #target: HasHealth;
+
+  constructor(source: Healer, target: HasHealth) {
+    this.#source = source;
+    this.#target = target;
+  }
+
   perform(): void {
-    if (!this.target.isAlive) {
+    if (!this.#target.isAlive) {
       throw new InvalidTargetError("cannot heal dead characters");
-    } else if (this.target !== (this.source as unknown)) {
+    } else if (this.#target !== (this.#source as unknown)) {
       throw new InvalidTargetError("a character can only heal themselves");
     }
 
-    this.target.health += this.source.healPower;
+    this.#target.health += this.#source.healPower;
   }
 }
