46f93d5aa81b249bb441d4abdf68156db72a98ac
Belén Albeza <belen@hey.com>
2023-11-02 11:16:02 +0100

    Step 1: basic attack

--- 2023-11-02-clj/src/rpg_combat/core.clj
+++ 2023-11-02-clj/src/rpg_combat/core.clj
@@ -1,8 +1,19 @@
 (ns rpg-combat.core
   (:gen-class))
 
-(defn character [name]
-  {:name name :health 1000})
+(defn character [name & {:keys [health] :or {health 1000}}]
+  {:name name :health health})
+
+(defn is-alive [chara]
+  (> (:health chara) 0))
+
+(defn- add-health [chara delta]
+  (assoc chara :health  (+ (:health chara) delta)))
+
+
+(defn attack [source target damage]
+  (let [updated-target (add-health target (- damage))]
+    [source updated-target]))
 
 (defn -main
   [& args]
--- 2023-11-02-clj/test/rpg_combat/core_test.clj
+++ 2023-11-02-clj/test/rpg_combat/core_test.clj
@@ -6,3 +6,21 @@
   (testing "character gets created with 1000 health"
     (let [c (character "Garrosh")]
       (is (= (:health c) 1000)))))
+
+(deftest character-is-alive
+  (testing "returns true when health is > 0"
+    (let [c (character "Garrosh")]
+      (is (true? (is-alive c)))))
+  (testing "returns false when health is zero"
+    (let [c (character "Garrosh" :health 0)]
+      (is (false? (is-alive c))))))
+
+(deftest character-attack
+  (testing "a character deals damage to another character"
+    (let [
+      src (character "Garrosh")
+      target (character "Elf" :health 1000)
+      damage 50
+      [src, target]  (attack src target damage)]
+
+      (is (= (:health target) 950)))))
