18867dfdae8eb4b6ddb60700283e6a4ae5633892
Belén Albeza <belen@hey.com>
2023-11-08 10:56:25 +0100

    Step 4: update main to reflect new actions

--- 2023-11-02-clj/src/rpg_combat/core.clj
+++ 2023-11-02-clj/src/rpg_combat/core.clj
@@ -5,7 +5,8 @@
     [rpg-combat.actions.heal :as actions.heal]
     [rpg-combat.actions.item :as actions.item]
     [rpg-combat.factions.faction-manager :as factions]
-    [rpg-combat.items.potion :as potion])
+    [rpg-combat.items.potion :as potion]
+    [rpg-combat.items.weapon :as weapon])
   (:gen-class))
 
 (defn -main
@@ -18,14 +19,20 @@
     fm (factions/join fm :horde orc)
     [orc elf outcome] (actions.attack/attack orc elf 50)
     _ (println (:id orc ) "attacks" (:id elf) "and deals" (:damage outcome) "damage!")
-    _ (println (str orc))
-    _ (println (str elf))
+    _ (println "\t" (str orc))
+    _ (println "\t" (str elf))
     p (potion/potion :health 10)
-    [elf p] (actions.item/use-item elf p)
     _ (println (:id elf) "drinks healing potion" (:health p) "hp")
-    _ (println (str p))
-    _ (println (str elf))
-    ;; [p elf] (potion/drink p (partial chara/add-health elf))
-    ;; _ (println (:id elf) "drinks potion for" hp "hp!")
-    ;; _ (println (str elf))
+    [elf p] (actions.item/use-item p elf)
+    _ (println "\tPotion:" (str p))
+    _ (println "\t" (str elf))
+    w (weapon/weapon :health 2 :damage 100)
+    _ (println (:id orc) "uses weapon" (str w))
+    [orc elf w] (actions.item/use-item w orc elf)
+    _ (println "\tWeapon:" (str w))
+    _ (println "\t" (str orc))
+    _ (println "\t" (str elf))
+    [_ elf outcome] (actions.heal/heal elf elf 30)
+    _ (println (:id elf ) "heals for" (:hp outcome) "!")
+    _ (println "\t" (str elf))
     ]))
