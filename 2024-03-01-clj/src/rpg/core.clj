d761e693acd1475306eddec57db1bff4d4d7095b
Belén Albeza <belen@hey.com>
2024-03-01 16:44:41 +0100

    Step 1: Implement character attack

--- 2024-03-01-clj/src/rpg/core.clj
+++ 2024-03-01-clj/src/rpg/core.clj
@@ -1,17 +1,45 @@
 (ns rpg.core
   (:gen-class))
 
+(defprotocol HasID
+  (uid [self]))
+
 (defprotocol HasHealth
-  (alive? [self]))
+  (alive? [self])
+  (add-health [self delta]))
 
-(defrecord Chara [health]
+(defprotocol Attacker
+  (attack [self target damage]))
+
+(defprotocol Healer
+  (heal [self hp]))
+
+(defrecord Chara [id health]
+  HasID
+  (uid [self] (:id self))
   HasHealth
-  (alive? [self] (> (:health self) 0)))
+  (alive? [self] (> (:health self) 0))
+  (add-health [self delta] (assoc self :health (max 0 (+ (:health self) delta))))
+  Attacker
+  (attack [self target damage]
+    (assert (and (satisfies? HasID target) (satisfies? HasHealth target) "invalid target"))
+    (when (= (uid self) (uid target)) (throw (Exception. "characters cannot attack themselves")))
+    (when (not (alive? self)) (throw (Exception. "dead characters cannot attack")))
+    (let [updated-target (add-health target (- damage))]
+      [self updated-target damage]))
+  Healer
+  (heal [self hp]
+    (let [self (add-health self hp)]
+      [self hp])))
 
-(defn character [& {:keys [health] :or {health 1000}}]
-  (->Chara health))
+(defn character [id & {:keys [health] :or {health 1000}}]
+  (->Chara id health))
 
 (defn -main
   "RPG Combat kata"
   [& args]
+  (let [
+    orc (character :garrosh)
+    elf (character :malfurion)]
+    (attack orc elf 10))
   (println "RPG Combat kata"))
--- 2024-03-01-clj/test/rpg/core_test.clj
+++ 2024-03-01-clj/test/rpg/core_test.clj
@@ -3,16 +3,46 @@
             [rpg.core :refer :all]))
 
 (deftest character-creation
+  (testing "Character is created with the given ID")
   (testing "Character is created with a default of 1000 health"
-    (let [c (character)]
+    (let [c (character :any)]
       (is (= (:health c) 1000))))
   (testing "Character is created with the given health value"
-    (let [c (character {:health 123})]
+    (let [c (character :any {:health 123})]
       (is (= (:health c) 123)))))
 
 (deftest character-health
   (testing "Character returns whether they are alive or not"
-    (let [alive-chara (character {:health 1})
-          dead-chara (character {:health 0})]
+    (let [alive-chara (character :alive {:health 1})
+          dead-chara (character :dead {:health 0})]
       (is (= true (alive? alive-chara)))
       (is (= false (alive? dead-chara))))))
+
+(deftest character-attack
+  (testing "A character can deal damage to another one"
+    (let [c (character :attacker)
+          other (character :target {:health 100})
+          [_ other _] (attack c other 10)]
+      (is (= (:health other) 90))))
+
+  (testing "A character's health cannot be reduced below zero after an attack"
+    (let [c (character :attacker)
+          other (character :target {:health 20})
+          [_ other _]  (attack c other 25)]
+      (is (= (:health other) 0))))
+
+  (testing "A character cannot attack themselves"
+    (let [c (character :attacker)]
+      (is (thrown-with-msg? Exception #"cannot attack" (attack c c 10)))))
+
+  (testing "A dead character cannot attack"
+    (let [c (character :attacker {:health 0})
+          other (character :target)]
+      (is (thrown-with-msg? Exception #"cannot attack" (attack c other 10))))))
+
+(deftest character-healing
+  (testing "A character can heal themselves"
+    (let [c (character :healer, {:health 900})
+          [c hp] (heal c 10)]
+      (is (= (:health c) 910))
+      (is (= hp 10)))))
