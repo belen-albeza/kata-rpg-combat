e71835bd09e932ab7f394159d69616d562c98843
Belén Albeza <belen@hey.com>
2024-03-04 14:12:02 +0100

    Step 2: levels

--- 2024-03-01-clj/src/rpg/core.clj
+++ 2024-03-01-clj/src/rpg/core.clj
@@ -8,28 +8,40 @@
   (alive? [self])
   (add-health [self delta]))
 
+(defprotocol HasLevel
+  (level [self]))
+
 (defprotocol Attacker
   (attack [self target damage]))
 
 (defprotocol Healer
   (heal [self hp]))
 
-(def ^:const max-health 1000)
+(defrecord Chara [id health level])
 
-(defrecord Chara [id health]
+(defmulti max-health class)
+(defmethod max-health Chara [self] (if (>= (:level self) 6) 1500 1000))
+
+(extend-type Chara
   HasID
   (uid [self] (:id self))
+  HasLevel
+  (level [self] (:level self))
   HasHealth
   (alive? [self] (> (:health self) 0))
   (add-health [self delta]
-    (let [health (min (max 0 (+ (:health self) delta)) max-health)]
+    (let [health (min (max 0 (+ (:health self) delta)) (max-health self))]
       (assoc self :health health)))
   Attacker
-  (attack [self target damage]
+  (attack [self target base-damage]
     (assert (and (satisfies? HasID target) (satisfies? HasHealth target) "invalid target"))
     (when (= (uid self) (uid target)) (throw (Exception. "characters cannot attack themselves")))
     (when-not (alive? self) (throw (Exception. "dead characters cannot attack")))
-    (let [updated-target (add-health target (- damage))]
+    (let [
+      level-diff (- (:level self) (level target))
+      damage-modifier (if (>= level-diff 5) 1.5 (if (<= level-diff -5) 0.5 1.0))
+      damage (int (* base-damage damage-modifier))
+      updated-target (add-health target (- damage))]
       [self updated-target damage]))
   Healer
   (heal [self hp]
@@ -37,8 +49,10 @@
     (let [self (add-health self hp)]
       [self hp])))
 
-(defn character [id & {:keys [health] :or {health max-health}}]
-  (->Chara id health))
+(defn character [id & {:keys [health level] :or {level 1}}]
+  (let [c (->Chara id 1 level)
+        health (if (some? health) (min (max-health c) health) (max-health c))]
+    (assoc c :health health)))
 
 (defn -main
   "RPG Combat kata"
--- 2024-03-01-clj/test/rpg/core_test.clj
+++ 2024-03-01-clj/test/rpg/core_test.clj
@@ -3,20 +3,37 @@
             [rpg.core :refer :all]))
 
 (deftest character-creation
-  (testing "Character is created with the given ID")
-  (testing "Character is created with a default of 1000 health"
+  (testing "Character is created with the given ID"
+    (let [c (character :foo)]
+      (is (= (:id c) :foo))))
+
+  (testing "Character is created with a default of 1000 health and level 1"
     (let [c (character :any)]
-      (is (= (:health c) 1000))))
+      (is (= (:health c) 1000))
+      (is (= (:level c) 1))))
+
   (testing "Character is created with the given health value"
     (let [c (character :any {:health 123})]
-      (is (= (:health c) 123)))))
+      (is (= (:health c) 123))))
+
+  (testing "Character is created with the given level")
+    (let [c (character :any {:level 5})]
+      (is (= (:level c) 5))))
 
 (deftest character-health
   (testing "Character returns whether they are alive or not"
     (let [alive-chara (character :alive {:health 1})
           dead-chara (character :dead {:health 0})]
       (is (= true (alive? alive-chara)))
-      (is (= false (alive? dead-chara))))))
+      (is (= false (alive? dead-chara)))))
+
+  (testing "Maximum health is 1000 for level 5 and lower"
+    (let [c (character :any {:level 5})]
+      (is (= (:health c) 1000))))
+
+  (testing "Maximum health is 1500 for level 6 and higher")
+    (let [c (character :any {:level 6})]
+      (is (= (:health c) 1500))))
 
 (deftest character-attack
   (testing "A character can deal damage to another one"
@@ -38,7 +55,19 @@
   (testing "A dead character cannot attack"
     (let [c (character :attacker {:health 0})
           other (character :target)]
-      (is (thrown-with-msg? Exception #"cannot attack" (attack c other 10))))))
+      (is (thrown-with-msg? Exception #"cannot attack" (attack c other 10)))))
+
+  (testing "Characters deal +50% damage when they attack a target that is 5 levels lower or more"
+    (let [c (character :attacker {:level 6})
+          other (character :target {:level 1 :health 1000})
+          [_ other _] (attack c other 100)]
+          (is (= (:health other) 850))))
+
+  (testing "Characters deal -50% damage when they attack a target that is 5 levels higher or more"
+    (let [c (character :attacker {:level 1})
+          other (character :target {:level 6 :health 1000})
+          [_ other _] (attack c other 100)]
+          (is (= (:health other) 950)))))
 
 (deftest character-healing
   (testing "A character can heal themselves"
