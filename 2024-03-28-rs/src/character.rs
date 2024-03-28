4b9919f7e4c302532d64f6838fb5c1ac79196cd1
Belén Albeza <belen@hey.com>
2024-03-28 15:47:20 +0100

    Step 1: add convenience chararacter builder

--- 2024-03-28-rs/src/character.rs
+++ 2024-03-28-rs/src/character.rs
@@ -1,11 +1,12 @@
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Character {
     health: u64,
+    damage: u64,
 }
 
 impl Character {
-    pub fn new(health: u64) -> Self {
-        Self { health }
+    pub fn new() -> Self {
+        Self::default()
     }
 
     pub fn alive(&self) -> bool {
@@ -15,13 +16,51 @@ impl Character {
 
 impl Default for Character {
     fn default() -> Self {
-        Self::new(1000)
+        Self {
+            health: 1000,
+            damage: 100,
+        }
+    }
+}
+
+pub struct CharacterBuilder {
+    health: u64,
+    damage: u64,
+}
+
+impl CharacterBuilder {
+    pub fn new() -> Self {
+        Self {
+            health: 1000,
+            damage: 1,
+        }
+    }
+
+    pub fn with_health(&mut self, health: u64) -> &mut Self {
+        self.health = health;
+        self
+    }
+
+    pub fn with_damage(&mut self, damage: u64) -> &mut Self {
+        self.damage = damage;
+        self
+    }
+
+    pub fn build(&self) -> Character {
+        let mut c = Character::new();
+        c.health = self.health;
+        c.damage = self.damage;
+        c
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn any_character_with_health(health: u64) -> Character {
+        CharacterBuilder::new().with_health(health).build()
+    }
 
     #[test]
     pub fn creates_character_with_default_values() {
@@ -31,10 +70,10 @@ mod tests {
 
     #[test]
     pub fn returns_whether_they_are_alive() {
-        let dead_chara = Character::new(0);
+        let dead_chara = any_character_with_health(0);
         assert_eq!(dead_chara.alive(), false);
 
-        let alive_chara = Character::new(1);
+        let alive_chara = any_character_with_health(1);
         assert_eq!(alive_chara.alive(), true);
     }
 }
--- 2024-03-28-rs/src/lib.rs
+++ 2024-03-28-rs/src/lib.rs
@@ -1,8 +1,13 @@
+use crate::character::CharacterBuilder;
+
 mod character;
 
 pub fn run() {
     println!("RPG Combat");
 
-    let orc = character::Character::default();
+    let orc = CharacterBuilder::new()
+        .with_health(800)
+        .with_damage(100)
+        .build();
     println!("\tOrc: {:?}", orc);
 }
